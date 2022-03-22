0c77b59909f49b17e492c423466e67ceda189202
Pawel Dera <paweldera01@gmail.com>
2022-03-22 17:53:00 +0100

    Added calculator class

---
+++ concurrent_programming/concurrent_programming/Calculator.cs
@@ -0,0 +1,34 @@
+﻿namespace concurrent_programming
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        } 
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Cannot divide by 0", "b");
+            }
+            return a / b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
